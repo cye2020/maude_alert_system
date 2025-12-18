ff39d8a56fe6bdea3c8c1b3990e55c521577835f
kyuree930 <gyulig373@gmail.com>
2025-12-18 21:16:31 +0900

    Feature/eda kim (#23)
    * Feature/preprocess cho (#2)
    
    * [feat] 전처리 통합 파일 생성
    
    * [feat] 다운로드 코드 추가
    
    * [feat] convert.py의 성능 최적화
    multiprocessing 추가
    
    * [feat] 메모리 절약 다운
    
    * [feat] 다운로드 class 생성
    
    * [feat] na_filter및 제품군 확인
    
    * 날짜 지우기 전 기록
    
    * [fix] load 삭제
    
    * Feature/preprocess shin (#11)
    
    * [feat] 전처리 통합 파일 생성
    
    * [feat] 다운로드 코드 추가
    
    * [feat] convert.py의 성능 최적화
    multiprocessing 추가
    
    * [feat] 메모리 절약 다운
    
    * [feat] 다운로드 class 생성
    
    * [데이터 적재 추가] (#1)
    
    * [feat] 다운로드 코드 추가
    
    * [feat] convert.py의 성능 최적화
    multiprocessing 추가
    
    * [feat] 메모리 절약 다운
    
    * [feat] 다운로드 class 생성
    
    * [feat] na_filter및 제품군 확인
    
    * 날짜 지우기 전 기록
    
    * [fix] load 삭제
    
    * Feature/preprocess cho (#3)
    
    * [feat] 다운로드 코드 추가
    
    * [feat] convert.py의 성능 최적화
    multiprocessing 추가
    
    * [feat] 메모리 절약 다운
    
    * [feat] 다운로드 class 생성
    
    * [feat] na_filter및 제품군 확인
    
    * 날짜 지우기 전 기록
    
    * [fix] load 삭제
    
    * [fix] Circular import 해결
    
    * [fix] notebook 로컬 모듈 불러오기 해결
    
    * 임시로 다 올림
    
    * vscode 제외
    
    * [feat] 전처리 함수 추가 (#7)
    
    전처리 유틸 함수 추가 및 데이터 다운 파일 수정 중
    
    * [feat] 전처리 함수 추가 (#8)
    
    전처리 함수 추가 및ㅊ 다운로드 수정
    
    * Feature/preprocess cho (#9)
    
    * [feat] 전처리 함수 추가
    
    * [feat] 다운로드 udi 도 가능하게 임시로 추가, 전처리 함수들 추가
    
    * [fix] 다운로드 모든 스키마 스캔
    
    * 1차 전처리
    
    * Move preprocess_s.ipynb iinto notebooks folder
    
    * Move preprocess_s.ipynb to notebooks folder
    
    * Delete preprocess_s.ipynb
    
    * Delete notebooks/중복 확인.ipynb
    
    * Sync notebooks folder
    
    * DS_Store 삭제
    
    * DS_Store 추가
    
    ---------
    
    Co-authored-by: cye2020 <ariaplay101@gmail.com>
    Co-authored-by: cye2020 <79790831+cye2020@users.noreply.github.com>
    
    * 분석내용 1 eda 및 기초통계
    
    * update EDA notebook
    
    * Ignore local sample CSV
    
    * 죄송합니다ㅠㅜ EDA 1218
    
    ---------
    
    Co-authored-by: cye2020 <79790831+cye2020@users.noreply.github.com>
    Co-authored-by: 유림 <urim108108@gmail.com>
    Co-authored-by: cye2020 <ariaplay101@gmail.com>

---
+++ code/__init__.py
@@ -0,0 +1,6 @@
+from .loading import DataLoader, ZipStreamer, Flattener, SchemaCollector, ParquetWriter
+
+
+__all__ = [
+    'DataLoader', 'ZipStreamer', 'Flattener', 'SchemaCollector', 'ParquetWriter'
+]
