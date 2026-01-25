04b8d347d58e2659e6652310a121db91dd6deaeb
cye2020 <ariaplay101@gmail.com>
2026-01-25 15:22:25 +0900

    [fix] 테이블 스키마 얻는 메서드 수정

--- lib/src/maude_early_alert/loading/snowflake_load.py
+++ lib/src/maude_early_alert/loading/snowflake_load.py
@@ -1,77 +1,6 @@
-<<<<<<< HEAD
-import logging
-from typing import List, Dict, Optional
-from dataclasses import dataclass
-import snowflake.connector
-from snowflake.connector import SnowflakeConnection
-
-@dataclass
-class ColumnInfo:
-    """테이블 컬럼 정보"""
-    name: str
-    data_type: str
-    is_nullable: bool
-    ordinal_position: int
-    
-logger = logging.getLogger(__name__)
-
-class SnowflakeLoader:
-    def __init__(self, database: str, schema: str, client: None):
-        self.database = database
-        self.schema = schema
-        self.client = client
-
-    def table_schema(self, table_name:str) -> List[ColumnInfo]:
-        """
-        Snowflake 테이블 스키마 조회
-        
-        Args:
-            table_name: 테이블 이름
-            
-        Returns:
-            List[ColumnInfo]: 컬럼 정보 리스트
-        """
-        query = f"""
-        select
-            column_name,
-            data_type,
-            is_nullable,
-            ordinal_position
-        from {self.database}.INFORMATION_SCHEMA.COLUMNS
-        where table_schema = '{self.schema}'
-        and table_name = '{table_name}'
-        order by ordinal_position
-        """
-        cursor = self.client.cursor()
-
-        try:
-            logger.info(f"스키마 조회:{self.database}.{self.schema}.{table_name}")
-            cursor.execute(query)
-            rows = cursor.fetchall()
-
-            if not rows:
-                raise ValueError(f"테이블 {self.database}.{self.schema}.{table_name} 스키마 조회 실패")
-
-            columns = [
-                ColumnInfo(
-                    name=row[0],
-                    type=row[1],
-                    is_nullable=(row[2] == 'YES'),
-                    ordinal_default=row[3]
-                )
-                for row in rows
-            ]
-            logger.info(f"{len(columns)}개의 컬럼 조회 완료")
-            return columns
-        finally:
-            cursor.close()
-
-    def load_from_s3(self, table_name: str, stg_name: str):
-        #1. 임시테이블생성
-        #2. copy into 문 실행
-        #3. merge
-=======
+from typing import List, Tuple
 from contextlib import contextmanager
+import snowflake.connector
 
 def get_staging_table_name(table_name: str) -> str:
     """Staging 테이블명 생성
@@ -85,7 +14,6 @@ def get_staging_table_name(table_name: str) -> str:
     parts = table_name.split('.')
     parts[-1] = f"stg_{parts[-1]}"
     return '.'.join(parts)
-
 
 class SnowflakeLoader:
     def __init__(self, database, schema, cursor = None):
@@ -107,6 +35,33 @@ class SnowflakeLoader:
     
     def load_merge(self):
         pass
+    
+    def get_table_schema(self, table_name:str) -> List[Tuple[str, str]]:
+        """
+        Snowflake 테이블 스키마 조회
+        
+        Args:
+            table_name: 테이블 이름
+            
+        Returns:
+            List[ColumnInfo]: 컬럼 정보 리스트
+        """
+        query = f"""
+        SELECT
+            column_name,
+            data_type
+        FROM {self.database}.INFORMATION_SCHEMA.COLUMNS
+        WHERE table_schema = '{self.schema}'
+            AND table_name = '{table_name}'
+        """
+
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+
+        if not rows:
+            raise ValueError(f"테이블 {self.database}.{self.schema}.{table_name} 스키마 조회 실패")
+
+        return rows
     
     @contextmanager
     def snowflake_transaction(self):
@@ -154,8 +109,11 @@ if __name__=='__main__':
             schema=secret['schema']       # Optional
         )
         # Create a cursor object
-        cur = conn.cursor()
+        cursor = conn.cursor()
+        
+        snowflake_loader = SnowflakeLoader(secret['database'], secret['schema'], cursor)
+        table_schema = snowflake_loader.get_table_schema('EVENT')
+        print(table_schema)
         
     except Exception as e:
         raise Exception(e)
->>>>>>> dffd07776c36b87873b051e2ee72204d4a0bbb35
