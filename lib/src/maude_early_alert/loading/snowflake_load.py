dbea0f3304ffb888456a1ef167174e56b26b9024
cye2020 <ariaplay101@gmail.com>
2026-01-25 15:36:14 +0900

    [feat] 임시 테이블 생성

--- lib/src/maude_early_alert/loading/snowflake_load.py
+++ lib/src/maude_early_alert/loading/snowflake_load.py
@@ -1,25 +1,26 @@
 from typing import List, Tuple
 from contextlib import contextmanager
 import snowflake.connector
+from snowflake.connector import SnowflakeConnection
 
 def get_staging_table_name(table_name: str) -> str:
     """Staging 테이블명 생성
 
     Args:
-        table_name: 원본 테이블명 (예: maude.bronze.event)
+        table_name: 원본 테이블명
 
     Returns:
-        Staging 테이블명 (예: maude.bronze.stg_event)
+        Staging 테이블명
     """
     parts = table_name.split('.')
-    parts[-1] = f"stg_{parts[-1]}"
+    parts[-1] = f"STG_{parts[-1].upper()}"
     return '.'.join(parts)
 
 class SnowflakeLoader:
-    def __init__(self, database, schema, cursor = None):
+    def __init__(self, database, schema, client: SnowflakeConnection = None):
         self.database = database
         self.schema = schema
-        self.cursor = cursor
+        self.client = client
     
     def load_from_s3(self, table_name: str, stg_name: str):
         # 1. 임시 테이블 생성
@@ -27,8 +28,24 @@ class SnowflakeLoader:
         # 3. MERGE로 목적 테이블로 적재
         pass
     
-    def create_temporary_staging_table(self):
-        pass
+    def create_temporary_staging_table(self, table_name: str):
+        stg_table_name = get_staging_table_name(table_name)
+        
+        table_schema = self.get_table_schema(table_name)
+        column_defs = ", ".join([f"{col} {data_type}" for col, data_type in table_schema])
+        
+        cursor = self.client.cursor()
+        
+        try:
+            cursor.execute(f"""
+                CREATE OR REPLACE TEMPORARY TABLE {stg_table_name} (
+                    {column_defs}
+                )
+            """)
+        finally:
+            cursor.close()
+        
+        return stg_table_name
     
     def copy_into(self):
         pass
@@ -54,12 +71,16 @@ class SnowflakeLoader:
         WHERE table_schema = '{self.schema}'
             AND table_name = '{table_name}'
         """
-
-        self.cursor.execute(query)
-        rows = self.cursor.fetchall()
-
-        if not rows:
-            raise ValueError(f"테이블 {self.database}.{self.schema}.{table_name} 스키마 조회 실패")
+        cursor = self.client.cursor()
+        
+        try:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            
+            if not rows:
+                raise ValueError(f"테이블 {self.database}.{self.schema}.{table_name} 스키마 조회 실패")
+        finally:
+            cursor.close()
 
         return rows
     
@@ -77,15 +98,16 @@ class SnowflakeLoader:
             - 성공 시 COMMIT
             - 예외 발생 시 ROLLBACK 후 재발생
         """
-        self.cursor.execute("BEGIN")
+        cursor = self.client.cursor()
+        cursor.execute("BEGIN")
         try:
-            yield self.cursor
-            self.cursor.execute("COMMIT")
+            yield cursor
+            cursor.execute("COMMIT")
         except Exception:
-            self.cursor.execute("ROLLBACK")
+            cursor.execute("ROLLBACK")
             raise
-
-    
+        finally:
+            cursor.close()
 
 if __name__=='__main__':
     import snowflake.connector
@@ -108,12 +130,12 @@ if __name__=='__main__':
             database=secret['database'],   # Optional
             schema=secret['schema']       # Optional
         )
-        # Create a cursor object
-        cursor = conn.cursor()
         
-        snowflake_loader = SnowflakeLoader(secret['database'], secret['schema'], cursor)
-        table_schema = snowflake_loader.get_table_schema('EVENT')
-        print(table_schema)
+        snowflake_loader = SnowflakeLoader(secret['database'], secret['schema'], conn)
+        stg_table_name = snowflake_loader.create_temporary_staging_table('EVENT')
+        print(stg_table_name)
+        stg_table_schema = snowflake_loader.get_table_schema(stg_table_name)
+        print(stg_table_schema)
         
     except Exception as e:
         raise Exception(e)
